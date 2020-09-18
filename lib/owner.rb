eeece09ee11fdff984da0a70e3193715249d3d46
Tim Flood <tflood9@gmail.com>
2020-09-17 23:51:59 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,8 +48,20 @@ class Owner
     new_dog = Dog.new(dog, self)
   end
 
-  def walks_the_dog
-
+  def walk_dogs
+    dogs.each {|dog| dog.mood = "happy"}
   end
 
+  def feed_cats
+    cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    cats.each {|cat| cat.mood = "anxious" && cat.owner = nil}
+    dogs.each {|dog| dog.mood = "anxious" && dog.owner = nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
